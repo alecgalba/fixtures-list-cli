dbffd691ced8ca3d46c65e1a2f8347f1faf03bfa
Alec Alba <alecgalba@gmail.com>
2017-08-13 15:08:16 +0000

    modified bin to hardcode info

--- lib/fixtures_list/cli.rb
+++ lib/fixtures_list/cli.rb
@@ -1,7 +1,37 @@
 class FixturesList::CLI
 
   def call
-    puts "Barcelona-lay, Barcelona-lay, Barcelona ale' ale' ale'"
+    list
+    menu
+    quit
+  end
+
+  def list
+    puts "
+    1. Premier League
+    2. La Liga
+    3. Serie A"
+  end
+
+  def menu
+    input = nil
+    while input != "exit"
+      puts ""
+      puts "Enter the number of the league to see the fixtures list. Enter 'exit' to blow the final whistle"
+      input = gets.strip.downcase
+      case input
+      when "1"
+        puts "Premier League fixtures"
+      when "2"
+        puts "La Liga fixtures"
+      when "3"
+        puts "Serie A fixtures"
+      end
+    end
+  end
+
+  def quit
+    puts "The referee blows the final whistle"
   end
 
 end
