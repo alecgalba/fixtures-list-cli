cfe54d386a47dc725b1290c14fa4c24eb9cb7260
Alec Alba <alecgalba@gmail.com>
2017-08-13 15:24:59 +0000

    updated cli for basic structure

--- lib/fixtures_list/cli.rb
+++ lib/fixtures_list/cli.rb
@@ -1,12 +1,12 @@
 class FixturesList::CLI
 
   def call
-    list
+    list_leagues
     menu
     quit
   end
 
-  def list
+  def list_leagues
     puts "
     1. Premier League
     2. La Liga
@@ -17,7 +17,7 @@ class FixturesList::CLI
     input = nil
     while input != "exit"
       puts ""
-      puts "Enter the number of the league to see the fixtures list. Enter 'exit' to blow the final whistle"
+      puts "Enter the number of the league to see the fixtures list. Enter 'list' to see the available leagues again. Enter 'exit' to blow the final whistle"
       input = gets.strip.downcase
       case input
       when "1"
@@ -26,6 +26,10 @@ class FixturesList::CLI
         puts "La Liga fixtures"
       when "3"
         puts "Serie A fixtures"
+      when "list"
+        list_leagues
+      else
+        puts "Not a valid option. Enter list to see your choices or exit to blow the whistle."
       end
     end
   end
