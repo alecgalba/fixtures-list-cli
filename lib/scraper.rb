be34d8f573e0cc8c173084c139e3d37e50a86ce6
Alec Alba <alecgalba@gmail.com>
2017-08-14 20:30:17 +0000

    modified to remove unnessary scraper data

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,11 +6,6 @@ class Scraper
       fixture_info = {
       #binding.pry
       :all_info => fixture.css("tbody").text.gsub(/\s+/, " ").strip
-      #:home_team => fixture.css("div.wisbb_team wisbb_reversed wisbb_firstTeam").text.strip,
-      #:away_team => fixture.css("div.wisbb_team wisbb_secondTeam").text.strip,
-      #:date => fixture.css("th").text.strip,
-      #:time => fixture.css("a").text.strip,
-      #:location => fixture.css("div.wisbb_location").text.strip
     }
 
       fixture_info
@@ -23,11 +18,6 @@ class Scraper
       fixture_info = {
         #binding.pry
         :all_info => fixture.css("tbody").text.gsub(/\s+/, " ").strip,
-        #:home_team => fixture.css("div.wisbb_team wisbb_reversed wisbb_firstTeam").text.gsub(/\s+/, " ").strip,
-        #:away_team => fixture.css("div.wisbb_team wisbb_secondTeam").text.gsub(/\s+/, " ").strip,
-        #:date => fixture.css("th").text.gsub(/\s+/, " ").strip,
-        #:time => fixture.css("a").text.gsub(/\s+/, " ").strip,
-        #:location => fixture.css("div.wisbb_location").text.gsub(/\s+/, " ").strip,
       }
 
       fixture_info
@@ -40,11 +30,6 @@ class Scraper
       fixture_info = {
         #binding.pry
         :all_info => fixture.css("tbody").text.gsub(/\s+/, " ").strip
-        #:home_team => fixture.css("div.wisbb_team wisbb_reversed wisbb_firstTeam").text.strip
-        #:away_team => fixture.css("div.wisbb_team wisbb_secondTeam").text.strip
-        #:date => fixture.css("th").text.strip
-        #:time => fixture.css("a").text.strip
-        #:location => fixture.css("div.wisbb_location")text.strip
       }
 
       fixture_info
