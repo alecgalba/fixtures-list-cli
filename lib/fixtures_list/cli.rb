682435ea51983a6b058ed4677d8c8e6251fb8b39
Alec Alba <alecgalba@gmail.com>
2017-08-14 05:34:23 +0000

    modified lib/fixtures_list/cli.rb for more interface detail?

--- lib/fixtures_list/cli.rb
+++ lib/fixtures_list/cli.rb
@@ -1,23 +1,53 @@
 class FixturesList::CLI
 
   def call
+    welcome
     list_leagues
     menu
     quit
   end
 
-  def list_leagues
+  def welcome
+    puts "Retrieving the English Premier League, Spanish La Liga, and Italian Serie A fixture schedules for you now."
+    puts ""
+    puts "..."
     puts "
-    1. Premier League
-    2. La Liga
-    3. Serie A"
+                ▓▓▓▓▓▓▓▓▓▓▓▓████████████
+            ▓▓▓▓▓▓▓▓███████▒▒▒▒▒▒▒▒▒███
+            ▓▓▓▓▓█████████▒▒▒▒▒▒▒▒▒▒▒▒████
+            ▓▓▓▓██▒███▒▒▒▒██▒▒▒▒▒▒▒▒▒█▒▒▒██
+            ▓▓▓█▒▒▒█▒▒▒▒▒▒▒▒█▒▒▒▒▒▒▒█▒▒▒▒▒▒██
+            ▓▓█▒▒▒█▒▒▒▒▒▒▒▒▒▒████████▒▒▒▒▒▒▒██
+            ▓█▒▒▒██▒▒▒▒▒▒▒▒▒█████████▒▒▒▒▒▒▒▒█
+            ██▒▒▒█▒▒▒▒▒▒▒▒▒▒██████████▒▒▒▒▒▒▒▒█
+            █▒▒▒██▒▒▒▒▒▒▒▒████████████▒▒▒▒▒▒▒▒█
+            █▒▒█████▒▒▒███▒▒▒███████▒▒████▒▒▒██
+            ███████████▒▒▒▒▒▒▒▒███▒▒▒▒▒▒▒▒█████
+            █▒███████▒▒▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒▒▒▒▒████
+            █▒███████▒▒▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒▒▒▒▒████
+            ▓█▒██████▒▒▒▒▒▒▒▒▒▒██▒▒▒▒▒▒▒▒▒▒███
+            ▓▓█▒██▒▒██▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒▒▒▒██▒██
+            ▓▓▓██▒▒▒▒▒███▒▒▒▒█████▒▒▒▒███▒▒▒█
+            ▓▓▓▓██▒▒▒▒▒▒███████████████▒▒▒██
+            ▓▓▓▓▓███▒▒▒▒▒▒██████████▒▒▒▒██
+            ▓▓▓▓▓▓▓████▒▒▒█████████▒▒███
+            ▓▓▓▓▓▓▓▓▓▓█████▒▒▒▒▒▒████ "
+  end
+
+  def list_leagues
+    sleep(10)
+    puts "
+    1. Premier League fixtures
+    2. La Liga fixtures
+    3. Serie A fixtures"
   end
 
   def menu
     input = nil
     while input != "exit"
       puts ""
-      puts "Enter the number of the league to see the fixtures list. Enter 'list' to see the available leagues again. Enter 'exit' to blow the final whistle"
+      puts "Enter the number of the league to see the fixtures list. Enter 'list' to see the available leagues again. Enter 'exit' to blow the final whistle:"
+      puts ""
       input = gets.strip.downcase
       case input
       when "1"
@@ -29,7 +59,8 @@ class FixturesList::CLI
       when "list"
         list_leagues
       else
-        puts "Not a valid option. Enter list to see your choices or exit to blow the whistle."
+        puts "Not a valid option. Enter list to see your choices or exit to blow the whistle:"
+        puts ""
       end
     end
   end
